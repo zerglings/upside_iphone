a87e44195cf927460c4594b220e311f20813c12f
Victor Costan <victor@zergling.net>
2009-01-02 18:30:01 -0500

    Initial commit: blank view application

---
+++ upside/Classes/upsideViewController.h
@@ -0,0 +1,16 @@
+//
+//  upsideViewController.h
+//  upside
+//
+//  Created by Victor Costan on 1/2/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface upsideViewController : UIViewController {
+
+}
+
+@end
+
