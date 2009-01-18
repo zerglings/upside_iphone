9e02e56c2cd78239857538871ed1897d859d0dc0
Victor Costan <victor@zergling.net>
2009-01-17 21:35:51 -0500

    Finishing touches to the Model Support testbed.

--- zn_testbed/app/controllers/web_support_controller.rb
+++ zn_testbed/app/controllers/web_support_controller.rb
@@ -1,8 +1,15 @@
 class WebSupportController < ApplicationController
+  protect_from_forgery :except => [:echo]
+  
   def echo
+    forbidden_hdrs = ['server_software'];
+    headers = request.headers.to_a.map { |hdr|
+         [hdr.first.to_s, hdr.last] }.select { |hdr| /^HTTP/ =~ hdr.first }
+    @headers = headers.map { |hdr| "#{hdr.first[5..-1]}: #{hdr.last}\n" }.join        
+         
     respond_to do |format|
       format.html # 
-      format.xml { render :xml => { :headers => request.headers,
+      format.xml { render :xml => { :headers => @headers,
                                     :method => request.method,
                                     :body => request.body.string } }
     end
