0512e1118796eb9a44386be35f1cdaebd6f98948
Victor Costan <victor@pwnage.local>
2009-06-29 15:41:06 -0400

    Changed version identifiers for 0.8 release.

---
+++ upside/CommControllers/UserUpdateCommController.h
@@ -0,0 +1,16 @@
+//
+//  UserUpdateCommController.h
+//  upside
+//
+//  Created by Victor Costan on 6/20/09.
+//  Copyright 2009 MIT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface UserUpdateCommController : NSObject {
+
+}
+
+@end
---
+++ upside/CommControllers/UserUpdateCommController.m
@@ -0,0 +1,14 @@
+//
+//  UserUpdateCommController.m
+//  upside
+//
+//  Created by Victor Costan on 6/20/09.
+//  Copyright 2009 MIT. All rights reserved.
+//
+
+#import "UserUpdateCommController.h"
+
+
+@implementation UserUpdateCommController
+
+@end
